6e11582de7e569e13244a5a546550cee0068663e
Denis <d.khokhloff2011@yandex.ru>
2023-12-18 14:28:03 +0300

    creating layout, connecting page navigation

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -1,3 +1,4 @@
+import { Layout } from '@/components/layout/layout.component';
 import { ROUTES } from './routes.data';
 import { NotFound } from '@/components/screens/not-found/not-found.component';
 
@@ -5,13 +6,40 @@ import { NotFound } from '@/components/screens/not-found/not-found.component';
  * Маршрутизатор
  */
 export class Router {
-	#routes;
-	#currentRoute;
+	#routes = ROUTES;
+	#currentRoute = null;
+	#layout = null;
 
 	constructor() {
-		this.#routes = ROUTES;
-		this.#currentRoute = null;
+		window.addEventListener('popstate', () => {
+			this.#handleRouteChange
+		})
+
 		this.#handleRouteChange();
+		this.#handleLinks();
+	}
+
+	/**
+	 * Обрабатывает все слушатели, если слушатель ссылка -
+	 * производит переадресацию через #handleRouteChange.
+	 */
+	#handleLinks() {
+		document.addEventListener('click', event => {
+			const target = event.target.closest('a');
+
+			if (target) {
+				event.preventDefault();
+
+				this.navigate(target.href)
+			}
+		})
+	}
+
+	navigate(path) {
+		if (path !== this.getCurrentPath()) {
+			window.history.pushState({}, '', path)
+			this.#handleRouteChange();
+		}
 	}
 
 	/**
@@ -29,7 +57,6 @@ export class Router {
 		const path = this.getCurrentPath() || '/';
 
 		let route = this.#routes.find(route => route.path === path);
-		console.log(route)
 
 		if (!route) {
 			route = {
@@ -38,12 +65,21 @@ export class Router {
 		}
 
 		this.#currentRoute = route;
-		this.render();
+		this.#render();
 	}
 
-	render() {
+	#render() {
 		const component = new this.#currentRoute.component();
 
-		document.getElementById('app').innerHTML = component.render();
+		if (!this.#layout) {
+			this.#layout = new Layout({
+				router: this,
+				children: component.render()
+			});
+
+			document.getElementById('app').innerHTML = this.#layout.render();
+		} else {
+			document.querySelector('main').innerHTML = component.render();
+		}
 	}
 }
