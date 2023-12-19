0461dffc222f17aa68b965b4471582f82e8b49fe
Denis <d.khokhloff2011@yandex.ru>
2023-12-19 14:04:07 +0300

    add function replaceComponentText

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,11 +1,15 @@
-import { BaseScreen } from "@/core/component/base-screen.component";
+import { BaseScreen } from '@/core/component/base-screen.component';
+import renderService from '@/core/services/render.service';
+import template from './home.template.html';
 
 export class Home extends BaseScreen {
-  constructor() {
-    super({title: 'Home'})
-  }
+	constructor() {
+		super({ title: 'Home' });
+	}
 
-  render() {
-    return '<p>Home</p>';
-  }
+	render() {
+		const element = renderService.htmlToElement(template);
+		return '<p>Home</p>'; 
+	}
 }
+ 
