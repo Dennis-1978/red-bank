d77dcb4f558b278edc9833a6b31230b7722dfb1b
Denis <d.khokhloff2011@yandex.ru>
2023-12-21 10:31:05 +0300

    Creating RQuery library, adding find and css methods

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -3,6 +3,7 @@ import renderService from '@/core/services/render.service';
 
 import template from './home.template.html';
 import styles from './home.module.scss';
+import { $R } from '@/core/rquery/rquery.lib';
 
 export class Home extends BaseScreen {
 	constructor() {
@@ -11,7 +12,9 @@ export class Home extends BaseScreen {
 
 	render() {
 		const element = renderService.htmlToElement(template, [], styles);
-		return element.outerHTML; 
+
+		$R(element).find('h1').css('color', 'red')
+
+		return element.outerHTML;
 	}
 }
- 
