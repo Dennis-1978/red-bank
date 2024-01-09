d72a056f5eb9e2af5b0050db146bdcaf771dab02
Denis <d.khokhloff2011@yandex.ru>
2024-01-09 13:31:23 +0300

    create logo, logout-button, search components in header, recreate auth in screen

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -3,10 +3,37 @@ import renderService from '@/core/services/render.service';
 
 import styles from './header.module.scss';
 import template from './header.template.html';
+import { Logo } from './logo/logo.component';
+import { LogoutButton } from './logout-button/logout-button.component';
+import { Search } from './search/search.component';
+import { UserItem } from '@/components/ui/user-item/user-item.component';
 
 export class Header extends ChildComponent {
+	constructor({router}) {
+		super();
+
+		this.router = router;
+	}
+
 	render() {
-		this.element = renderService.htmlToElement(template, [], styles);
+		this.element = renderService.htmlToElement(
+			template,
+			[
+				Logo,
+				new LogoutButton({router: this.router}),
+				Search,
+				new UserItem(
+					{
+						avatarPath:
+							'https://papik.pro/uploads/posts/2021-09/1631773650_15-papik-pro-p-prikolnie-risunki-vse-15.jpg',
+						name: 'Guffi',
+					},
+					false,
+					() => alert('Hey'),
+				),
+			],
+			styles,
+		);
 
 		return this.element;
 	}
--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,11 +1,8 @@
 import { BaseScreen } from '@/core/component/base-screen.component';
-import { $R } from '@/core/rquery/rquery.lib';
 import renderService from '@/core/services/render.service';
 
 import template from './home.template.html';
 import styles from './home.module.scss';
-import { Loader } from '@/components/ui/loader/loader.component';
-import { UserItem } from '@/components/ui/user-item/user-item.component';
 
 export class Home extends BaseScreen {
 	constructor() {
@@ -13,24 +10,7 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(
-			template,
-			[
-				new Loader(),
-				new UserItem(
-					{
-						avatarPath:
-							'https://papik.pro/uploads/posts/2021-09/1631773650_15-papik-pro-p-prikolnie-risunki-vse-15.jpg',
-						name: 'Guffi',
-					},
-					false,
-					() => alert('Hey'),
-				),
-			],
-			styles,
-		);
-
-		$R(element).find('h1').css('color', 'blue');
+		const element = renderService.htmlToElement(template, [], styles);
 
 		return element;
 	}
